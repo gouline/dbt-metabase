de7c0878228d729f811c48dddc46628a21f74610
keurcien <keurcien.luu@gmail.com>
2023-06-23 02:30:44 +0200

    MetabaseCard model

--- dbtmetabase/models/metabase.py
+++ dbtmetabase/models/metabase.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass, field
 from enum import Enum
-from typing import MutableMapping, Optional, Sequence
+from typing import MutableMapping, Optional, Sequence, List
 
 # Allowed metabase.* fields
 _METABASE_COMMON_META_FIELDS = [
@@ -72,6 +72,55 @@ class MetabaseModel:
         return None
 
     columns: Sequence[MetabaseColumn] = field(default_factory=list)
+
+
+
+@dataclass
+class MetabaseCard:
+    visualization_settings: MutableMapping = field(default_factory=dict)
+    parameters: Optional[List[MutableMapping]] = None
+    description: Optional[str] = None
+    collection_position: Optional[int] = None
+    result_metadata: Optional[List[MutableMapping]] = None
+    collection_id: Optional[int] = None
+    cache_ttl: Optional[int] = None
+    parameter_mappings: Optional[List[MutableMapping]] = None
+    display: Optional[str] = None
+
+    exposure_name: Optional[str] = None
+    unique_id: Optional[str] = None
+    depends_on: Optional[MutableMapping] = None
+    compiled_code: Optional[str] = None
+
+    @property
+    def id(self):
+        return int(self.exposure_name.split("-")[0].replace("MB", "").strip())
+    
+    @property
+    def name(self):
+        return self.exposure_name.split("-")[1].strip()
+    
+    @property
+    def analysis_id(self):
+        nodes = self.depends_on.get("nodes", [])
+        assert len(nodes) == 1, "Metabase card should only depend on one analysis model."
+        return nodes[0]
+    
+    def json(self, database_id: int):
+        return {
+            "name": self.name,
+            "description": self.description,
+            "display": "table",
+            "visualization_settings": {},
+            "dataset_query": {
+                "type": "native",
+                "native": {
+                    "query": self.compiled_code,
+                    "template_tags": {},
+                },
+                "database": database_id,
+            }
+        }
 
 
 class _NullValue(str):
